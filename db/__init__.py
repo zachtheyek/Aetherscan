a25c64fe7e6ba7daa5c4fe47c040babf23a35c86
zachtheyek <xiaoxiyek1.5@gmail.com>
2025-11-27 15:40:08 +0800

    Update db __init__ interface

--- db/__init__.py
+++ db/__init__.py
@@ -4,12 +4,14 @@ Database package for Aetherscan pipeline
 
 from .db import (
     get_db,
+    get_system_metadata,
     init_db,
     shutdown_db,
 )
 
 __all__ = [
     "get_db",
+    "get_system_metadata",
     "init_db",
     "shutdown_db",
 ]
