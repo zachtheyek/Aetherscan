1b3e0ba463bb4ae01f8df53a647d55812ce2bd64
zachtheyek <xiaoxiyek1.5@gmail.com>
2025-11-27 15:43:19 +0800

    Update monitor __init__ interface

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -4,12 +4,14 @@ Resource monitor package for Aetherscan pipeline
 
 from .monitor import (
     get_monitor,
+    get_process_tree_stats,
     init_monitor,
     shutdown_monitor,
 )
 
 __all__ = [
     "get_monitor",
+    "get_process_tree_stats",
     "init_monitor",
     "shutdown_monitor",
 ]
