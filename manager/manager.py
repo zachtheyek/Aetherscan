efcdf0df95fc99648450f7f58b016986f15d0790
zachtheyek <xiaoxiyek1.5@gmail.com>
2025-12-28 01:18:06 +0800

    Update todo's

--- manager/manager.py
+++ manager/manager.py
@@ -300,7 +300,7 @@ class ResourceManager:
         # sys.exit(1): exit with failed termination status
         sys.exit(0)
 
-    # TEST: does cleanup_all() run properly for all scenarios? ✅: normal exit, ❓: ctrl+c during setup/multiprocessing/distributed training/other, sys.exit() from main.py
+    # TEST: does cleanup_all() run properly for all scenarios? ✅: normal exit, ❓: ctrl+c during setup/multiprocessing/distributed training/other, sys.exit() from main.py, in between training retries
     def cleanup_all(self):
         """
         Unified cleanup of all resources.
